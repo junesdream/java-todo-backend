8008d41bc990b44673b9226103e7904fafebfcb7
junesdream <norisori@gmail.com>
2023-03-29 14:29:42 +0200

    Header & TodoCard added

--- frontend/src/components/TodoCard.tsx
+++ frontend/src/components/TodoCard.tsx
@@ -1,6 +1,7 @@
 import "./TodoCard.css"
 import {Todo} from "../model/Todo";
 import {Button} from "@mui/material";
+import {useNavigate} from "react-router-dom";
 
 type Props = {
     todo: Todo,
@@ -10,6 +11,7 @@ type Props = {
 
 export default function TodoCard(props: Props) {
 
+    const navigate = useNavigate()
     const nextTodoStatus: {
         OPEN: "IN_PROGRESS", IN_PROGRESS: "DONE", DONE: "OPEN" } = {
         OPEN: "IN_PROGRESS",
@@ -18,6 +20,7 @@ export default function TodoCard(props: Props) {
     }
 
     function onAdvanceClick() {
+
         const updatedTodo: Todo = {...props.todo, status: nextTodoStatus[props.todo.status]}
         props.updateTodo(updatedTodo)
     }
@@ -31,8 +34,13 @@ export default function TodoCard(props: Props) {
         <h5>{props.todo.description}</h5>
         <p>{props.todo.status}</p>
         <p>{props.todo.id}</p>
+        {/*<button onClick={() => {navigate('/todos/' + props.todo.id)}}>Details</button>*/}
+
+        <Button variant="outlined" sx={{ color: ' #50BFE6' }} onClick={() => {navigate('/todos/' + props.todo.id)}}> Details</Button>
         {props.todo.status !== 'DONE' && <Button variant="outlined" color="success" onClick={onAdvanceClick}>Advance</Button>}
-        {props.todo.status === 'DONE' && <Button variant="outlined" color="success" onClick={onDeleteClick}>Delete</Button>}
+        {props.todo.status === 'DONE' && <Button variant="outlined" sx={{ color: '#FF00CC' }} onClick={onDeleteClick}>Delete</Button>}
+
+
     </div>
 )
 }
